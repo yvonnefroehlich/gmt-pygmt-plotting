695e25c6b67e6d3479392792340d1d63da090b46
yvonnefroelich <yvonne.froehlich@kit.edu>
2024-05-04 12:08:18 +0200

    Adjust earthquake examples

--- 004_earthquakes_eruptions/07_taiwan/taiwan_earthquake_BFO.py
+++ 004_earthquakes_eruptions/07_taiwan/taiwan_earthquake_BFO.py
@@ -649,7 +649,7 @@ fig.shift_origin(xshift="-10c", yshift="4c")
 
 # Generate plot for travel paths with self-defined function
 taup_path(
-    fig=fig,
+    fig_instance=fig,
     fig_width="7c",
     max_dist=360,
     font_size="7p",
