32c6b9e295face1268dffeb819d82cbc2934623f
yvonnefroelich <yvonne.froehlich@kit.edu>
2024-11-26 13:23:01 +0100

    Add general example to revert font scaling for colorbar

---
+++ 000_general_stuff/03_cb_font_scaling/cb_font_scaling.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+# #############################################################################
+# Adjust font size of colorbar in GMT 6.5 to revert auto-scaling relatively to
+# colorbar length
+# -----------------------------------------------------------------------------
+# Author: Yvonne Fröhlich
+# ORCID: https://orcid.org/0000-0002-8566-0619
+# GitHub: https://github.com/yvonnefroehlich/gmt-pygmt-plotting
+# -----------------------------------------------------------------------------
+# - Created: 2024/11/26
+#   PyGMT v0.13.0 -> https://www.pygmt.org/v0.13.0/ | https://www.pygmt.org/
+#   GMT 6.5.0 -> https://www.generic-mapping-tools.org/
+# #############################################################################
+
+
+import pygmt
+import numpy as np
+
+
+plot_size = 5
+font_size = 6
+
+cb1_width = 4
+cb2_width = 1.5
+cb1_label = "cb1 is long"
+cb2_label = "cb2 is short"
+
+
+# -----------------------------------------------------------------------------
+def scale_cb_font(cb_width):
+    scale_factor = 1 / 15  # scale factor used in GMT 6.5
+    font_scaling = np.sqrt(cb_width * scale_factor)
+    return font_scaling
+# -----------------------------------------------------------------------------
+
+
+# -----------------------------------------------------------------------------
+fig = pygmt.Figure()
+fig.basemap(region=[-plot_size, plot_size] * 2, projection=f"X{plot_size}c", frame=0)
+
+fig.colorbar(cmap="navia", position=f"jTC+w{cb1_width}c+h", frame=f"x+l{cb1_label}")
+fig.colorbar(cmap="navia", position=f"jBC+w{cb2_width}c+h", frame=f"x+l{cb2_label}")
+
+# -----------------------------------------------------------------------------
+fig.shift_origin(xshift="5.5c")
+fig.basemap(region=[-plot_size, plot_size] * 2, projection=f"X{plot_size}c", frame=0)
+
+font_scaling = scale_cb_font(cb1_width)
+with pygmt.config(FONT=f"{font_size / font_scaling}"):
+    fig.colorbar(cmap="navia", position=f"jTC+w{cb1_width}c+h", frame=f"x+l{cb1_label}")
+
+font_scaling = scale_cb_font(cb2_width)
+with pygmt.config(FONT=f"{font_size / font_scaling}"):
+    fig.colorbar(cmap="navia", position=f"jBC+w{cb2_width}c+h", frame=f"x+l{cb2_label}")
+# -----------------------------------------------------------------------------
+
+fig.show()
--- 000_general_stuff/README.md
+++ 000_general_stuff/README.md
@@ -2,8 +2,9 @@
 
 ## Content
 
-_Recommended versions_: PyGMT v0.11.0 / v0.12.0, GMT 6.4.0 / 6.5.0
+_Recommended versions_: PyGMT v0.11.0 / v0.12.0 / v0.13.0, GMT 6.4.0 / 6.5.0
 
 - **[01_custom_symbols](https://github.com/yvonnefroehlich/gmt-pygmt-plotting/tree/main/000_general_stuff/01_custom_symbols)**
+- **[03_cb_font_scaling](https://github.com/yvonnefroehlich/gmt-pygmt-plotting/tree/main/000_general_stuff/03_cb_font_scaling)**
 
 ![](https://github.com/yvonnefroehlich/gmt-pygmt-plotting/raw/main/_images/github_maps_readme_000general.png)
