39e92b69b2be2d925fed948ef2d7fc6aa61c35ed
yvonnefroelich <yvonne.froehlich@kit.edu>
2024-12-01 15:14:51 +0100

    Add data and code for GMT

--- 011_agu_FTLJG_2024/FTLJG_agu24_u12b05_pygmt_numpy.py
+++ 011_agu_FTLJG_2024/FTLJG_agu24_u12b05_pygmt_numpy.py
@@ -29,23 +29,39 @@ import datetime
 import numpy as np
 
 # Data retrieved from https://star-history.com/#GenericMappingTools/pygmt&Date 2024/12/01
-x = np.array(
+dates_pygmt = np.array(
     ["2017-03-30", "2017-08-05", "2018-05-24", "2019-01-11", "2019-12-10", "2020-05-18",
      "2020-12-29", "2021-06-22", "2021-12-01", "2022-03-06", "2022-07-26", "2023-02-16",
      "2023-09-11", "2023-11-11", "2024-05-11", "2024-12-01"], dtype=np.datetime64
 )
-y = np.array([0, 30, 90, 150, 210, 240, 300, 360, 420, 450, 510, 570, 630, 660, 720, 769])
+stars_pygmt = np.array([
+    0, 30, 90, 150, 210, 240, 300, 360, 420, 450, 510, 570, 630, 660, 720, 769]
+)
+
+dates_gmt = np.array(
+    ["2018-08-13", "2019-01-04", "2019-03-01", "2019-07-27", "2019-11-29", "2020-03-13",
+     "2020-07-08", "2020-09-26", "2021-03-15", "2021-09-13", "2022-02-16", "2022-08-23",
+     "2023-04-13", "2023-11-17", "2024-05-29", "2024-12-01"], dtype=np.datetime64
+)
+stars_gmt = np.array([
+    0, 60, 120, 180, 240, 300, 360, 390, 450, 510, 570, 630, 690, 750, 810, 864]
+)
+
 
 fig = pygmt.Figure()
 fig.basemap(
     projection="X12c/6c",
-    region=[datetime.date(2017, 1, 1), datetime.date(2024, 12, 31), 0, 800],
+    region=[datetime.date(2017, 1, 1), datetime.date(2025, 6, 30), 0, 900],
     frame=["x+lyear", "y+lGitHub stars"],
 )
 
-fig.plot(x=x, y=y, pen="1p,255/222/87", no_clip=True)
-fig.plot(x=x, y=y, style="c0.25c", fill="238/86/52", pen="1.5p,63/124/173", no_clip=True)
+fig.plot(x=dates_gmt, y=stars_gmt, pen="238/86/52", no_clip=True)
+fig.plot(x=dates_gmt, y=stars_gmt, style="c0.23c", fill="238/86/52", no_clip=True, label="GMT")
 
+fig.plot(x=dates_pygmt, y=stars_pygmt, pen="63/124/173", no_clip=True)
+fig.plot(x=dates_pygmt, y=stars_pygmt, style="c0.23c", fill="63/124/173", no_clip=True, label="PyGMT")
+
+fig.legend(position="jBR")
 fig.show()
 
 
