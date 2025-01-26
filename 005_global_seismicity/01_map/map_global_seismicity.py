a8789254b395e4fffb16fefd298ee47236d1b7ce
yvonnefroelich <yvonne.froehlich@kit.edu>
2025-01-26 17:55:31 +0100

    Include orthographic projection

--- 005_global_seismicity/01_map/map_global_seismicity.py
+++ 005_global_seismicity/01_map/map_global_seismicity.py
@@ -35,20 +35,22 @@ import pandas as pd
 # >>> Set for your needs <<<
 
 # Set projection of map
-proj = "rob"  ## "rob", "ortho"
-fig_size = 12
+proj = "ortho"  ## "rob" | "epi" | "ortho"
+fig_size = 11   # in centimeters
+# epicentral distance
 lon_center = 6  # degrees East
 lat_center = 48  # degrees North
 epi_dist = 150  # degrees
+# orthographic
+lon0 = 60  # degrees East
+lat0 = 10  # degrees North
 
 # Request earthquake data
 # >>> Only works with single quotation marks <<<
-start_year = 2000
-end_year = 2023
-start_date = str(start_year) + '-01-01'
-end_date = str(end_year) + '-12-31'
-min_magnitude = '6'
-max_magnitude = '10'
+start_date = '2000-01-01'
+end_date = '2023-12-31'
+min_mag_w = '6'  # magnitude, later filter regarding moment magnitude
+max_mag_w = '10'
 order_records = 'time-asc'  # 'magnitude'
 
 # -----------------------------------------------------------------------------
@@ -61,7 +63,7 @@ file_legend = "legend_gmt_magitude.txt"
 # Plotting
 color_highlight = "255/90/0"
 col_land = "gray90"
-col_water = "steelblue@85"
+col_water = "lightskyblue"
 col_pb = "216.750/82.875/24.990"
 col_sl = "gray60"
 box_standard = "+gwhite@30+p0.8p,black+r2p"
@@ -70,8 +72,13 @@ clearance_standard = "0.1c/0.1c+tO"
 match proj:
     case "rob":
         proj_used = f"N{fig_size}c"
+        frame_used = "af"
+    case "epi":
+        proj_used = f"E{lon_center}/{lat_center}/{epi_dist}/{fig_size}c"
+        frame_used = "af"
     case "ortho":
-        proj_used =f"E{lon_center}/{lat_center}/{epi_dist}/{fig_size}c"
+        proj_used = f"G{lon0}/{lat0}/{fig_size}c"
+        frame_used = "afg"
 
 
 # %%
@@ -80,28 +87,25 @@ match proj:
 # -----------------------------------------------------------------------------
 # Set up request
 # see https://earthquake.usgs.gov/fdsnws/event/1/
-# last access: 2023/11/20
+# last access: 2025/01/26
 url_usgs = 'https://earthquake.usgs.gov/fdsnws/event/1/query.csv'
 
 url_usgs_request = url_usgs + '?' + \
     '&'.join([
         'starttime=' + start_date + '%2000:00:00',
         'endtime=' + end_date + '%2000:00:00',
-        'minmagnitude=' + min_magnitude,
-        'maxmagnitude=' + max_magnitude,
+        'minmagnitude=' + min_mag_w,
+        'maxmagnitude=' + max_mag_w,
         'orderby=' + order_records,
     ])
 
-eq_catalog_name = f"global_seismicity_{start_date}to{end_date}_mw" + \
-                   f"{min_magnitude}to{max_magnitude}"
+eq_catalog_name = f"global_seismicity_{start_date}to{end_date}_mw{min_mag_w}to{max_mag_w}"
 
 # Download data into a pandas DataFrame
 data_eq_raw = pd.read_csv(url_usgs_request)
 
 # Write data to a CSV file
-data_eq_raw.to_csv(
-    index=False, path_or_buf=f"{path_in}/data_{eq_catalog_name}.csv", sep="\t",
-)
+data_eq_raw.to_csv(path_or_buf=f"{path_in}/data_{eq_catalog_name}.csv", sep="\t", index=False)
 
 # Filter data
 # mw, mwc, mwb, mwr, mww
@@ -140,7 +144,7 @@ fig.plot(data=data_eq_used[epi_columns], cmap=True, style="cc", pen="0.01p,gray3
 with pygmt.config(FONT="14p"):
     fig.colorbar(
         frame=["xa100+lhypocentral depth", "y+lkm"],
-        position="JBC+o-2.6c/1.4c+w5c/0.3c+h+ml+ef0.2c",
+        position="JBC+o-2.5c/1.4c+w5c/0.3c+h+ml+ef0.2c",
         box=box_standard,
     )
 
@@ -148,7 +152,8 @@ with pygmt.config(FONT="14p"):
 fig.legend(spec=f"{path_in}{file_legend}", position="JBC+o3c/1.53c+w4c", box=box_standard)
 
 # Plot map frame on top
-fig.basemap(frame="af")
+pygmt.config(MAP_GRID_PEN_PRIMARY="0.01p,gray30")
+fig.basemap(frame=frame_used)
 
 # -----------------------------------------------------------------------------
 # Add label for time period
@@ -156,7 +161,7 @@ fig.text(
     text=f"{start_date} to {end_date}",
     font=color_highlight,
     position="BR",
-    offset="-1.2c/-1.2c",
+    offset="-0.6c/-1.2c",
     pen="0.8p,black",
     clearance=clearance_standard,
     no_clip=True,
@@ -166,7 +171,7 @@ fig.text(
 # Show and save figure
 fig.show()
 fig_name = f"map_{proj}_{eq_catalog_name}"
-# for ext in ["png"]:  # "pdf", "eps"
-#     fig.savefig(fname=f"{path_out}{fig_name}.{ext}", dpi=dpi_png)
+for ext in ["png"]:  # "pdf", "eps"
+    fig.savefig(fname=f"{path_out}{fig_name}.{ext}", dpi=dpi_png)
 
 print(fig_name)
