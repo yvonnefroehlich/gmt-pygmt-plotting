332c4fe53a393339307da5f98c73e2674567797b
yvonnefroelich <yvonne.froehlich@kit.edu>
2025-03-31 14:13:04 +0200

    Introduce function 'taup_symbol'

--- 003_taup/taup_path_curve.py
+++ 003_taup/taup_path_curve.py
@@ -31,6 +31,7 @@
 # - Udpated: 2025/03/27 - Maintenance: Adjust building file name
 # - Updated: 2025/03/29 - Enhancement: Introduce function taup_style
 # - Updated: 2025/03/30 - Enhancement: Allow setting step of epicentral distance annotations
+# - Updated: 2025/03/31 - Enhancement: Introduce function taup_symbol
 # #############################################################################
 
 
@@ -40,7 +41,7 @@ from obspy.taup import TauPyModel
 
 from taup_color import taup_color
 from taup_style import taup_style
-
+from taup_symobl import taup_symbol
 
 def taup_path(
     source_depth,
@@ -136,11 +137,13 @@ def taup_path(
     color_highlight = "255/90/0"
     box_standard = "+gwhite+p0.1p,gray30+r2p"
 
-    # Line colors and styles for the seismological phases
-    # Adjust and extend the dictionary for your needs in taup_color.py
+    # Seismological phases
+    # Colors: adjust and extend the dictionary for your needs in taup_color.py
     phase_colors = taup_color()
-    # Adjust and extend the dictionary for your needs in taup_style.py
+    # Line styles: adjust and extend the dictionary for your needs in taup_style.py
     phase_styles = taup_style()
+	# Symbols: adjust and extend the dictionary for your needs in taup_symbol.py
+    phase_symbol = taup_symbol()
 
     # %%
     # -------------------------------------------------------------------------
@@ -375,7 +378,7 @@ def taup_path(
             fig_curve.plot(
                 x=receiver_dist,
                 y=phase_time_split[0],
-                style="c0.13c",
+                style=f"{phase_symbol[phase_label_split[0]]}0.13c",
                 fill=phase_colors[phase_label_split[0]],
                 pen="0.001p,gray10",
                 no_clip=True,
@@ -389,7 +392,7 @@ def taup_path(
                     fig_curve.plot(
                         x=-1,
                         y=-1,
-                        style="c0.2c",
+                        style=f"{phase_symbol[phase_label_split[0]]}0.2c",
                         fill=phase_colors[phase],
                         pen="0.05p,gray10",
                         label=f"{phase}{info_str}{col_str}+S0.15c",
@@ -540,8 +543,8 @@ fig_path, fig_curve = taup_path(
     max_dist=360,
     phases=["S", "ScS", "PKS", "PKKS", "SKS", "SKKS", "SKIKS", "SKJKS"],
     time_curve=True,
-    # fig_save=True,
-    # save_path="02_your_example_figures/",
+    fig_save=True,
+    save_path="02_your_example_figures/",
 )
 
 fig_path = taup_path(
@@ -555,8 +558,8 @@ fig_path = taup_path(
     min_depth=660,
     max_depth=4000,
     phases=["S", "ScS", "PKS", "PKKS", "SKS", "SKKS", "SKIKS", "SKJKS"],
-    # fig_save=True,
-    # save_path="02_your_example_figures/",
+    fig_save=True,
+    save_path="02_your_example_figures/",
 )
 
 # -----------------------------------------------------------------------------
@@ -568,8 +571,8 @@ fig_path = taup_path(
     min_dist=-5,
     max_dist=100,
     phases=["SKS", "pSKS", "sSKS", "SKKS", "pSKKS", "sSKKS"],
-    # fig_save=True,
-    # save_path="02_your_example_figures/",
+    fig_save=True,
+    save_path="02_your_example_figures/",
 )
 
 fig_path = taup_path(
@@ -582,8 +585,8 @@ fig_path = taup_path(
     min_depth=0,
     max_depth=4000,
     phases=["SKS", "pSKS", "sSKS", "SKKS", "pSKKS", "sSKKS"],
-    # fig_save=True,
-    # save_path="02_your_example_figures/",
+    fig_save=True,
+    save_path="02_your_example_figures/",
 )
 
 fig_path = taup_path(
@@ -597,8 +600,8 @@ fig_path = taup_path(
     min_depth=0,
     max_depth=800,
     phases=["SKS", "pSKS", "sSKS", "SKKS", "pSKKS", "sSKKS"],
-    # fig_save=True,
-    # save_path="02_your_example_figures/",
+    fig_save=True,
+    save_path="02_your_example_figures/",
 )
 
 
@@ -637,6 +640,6 @@ for dist in np.arange(dist_min, dist_max + dist_step, dist_step):
         curve_dist_range=[dist_min - dist_step, dist_max + dist_step],
         curve_time_range=[0, 2700],
         legend_path=False,
-        # fig_save=fig_save,
-        # save_path="02_your_example_figures/",
+        fig_save=fig_save,
+        save_path="02_your_example_figures/",
     )
---
+++ 003_taup/taup_symbol.py
@@ -0,0 +1,59 @@
+# #############################################################################
+# This functions
+# - Assigns symbols to the seismological phases
+# - Outputs a dictionary
+# - Is related to the function taup_path_curve.py
+# Feel free to adjust and extend the dictionary for your needs (:
+# -----------------------------------------------------------------------------
+# Author: Yvonne Fröhlich
+# ORCID: https://orcid.org/0000-0002-8566-0619
+# GitHub: https://github.com/yvonnefroehlich/gmt-pygmt-plotting
+# -----------------------------------------------------------------------------
+# Related to:
+#   Fröhlich Y., Grund M. & Ritter J. R. R. (2024).
+#   Lateral and vertical variations of seismic anisotropy in the lithosphere-
+#   asthenosphere system underneath Central Europe from long-term splitting
+#   measurements. Geophysical Journal International, 239(1), 112-135.
+#   https://doi.org/10.1093/gji/ggae245.
+# -----------------------------------------------------------------------------
+# - Created: 2025/03/31
+# #############################################################################
+
+
+def taup_symbol():
+
+    phase_symbols = {
+        "P": "s",
+        "PcP": "c",
+        "S": "c",
+        "ScS": "c",
+        "PKS": "c",
+        "PKKS": "c",
+        "SKS": "c",
+        "SKKS": "c",
+        "PKKP": "c",
+        "PKPPKP": "c",
+        "PKIKS": "c",
+        "PKJKS": "c",
+        "SKIKS": "c",
+        "SKJKS": "c",
+        # Deth phases for SKS and SKKS phases
+        "pSKS": "c",
+        "sSKS": "c",
+        "pSKKS": "c",
+        "sSKKS": "c",
+    }
+
+    # Depth phases for PcP and ScS phases
+    # phase_symbols = {
+    #     "P": "c",
+    #     "PcP": "c",
+    #     "pPcP": "c",
+    #     "sPcP": "c",
+    #     "S": "c",
+    #     "ScS": "c",
+    #     "pScS": "c",
+    #     "sScS": "c",
+    # }
+
+    return phase_symbol
