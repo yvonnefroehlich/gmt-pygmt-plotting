c17e9b8cdeaad69635c81208b742947f6d8ae1d5
Yvonne Fröhlich <yvonne.froehlich@kit.edu>
2025-08-02 17:44:48 +0200

    Add code

---
+++ 010_axisem/06_nr_field/nr_field.py
@@ -0,0 +1,257 @@
+# #############################################################################
+# AxiSEM3D - Nr field
+# - wavefield scanning / learning
+# - complexity map
+# - Cartesian histogram
+# -----------------------------------------------------------------------------
+# History
+# - Created: 2025/08/02
+# -----------------------------------------------------------------------------
+# Versions
+# - PyGMT v0.16.0 -> https://www.pygmt.org/v0.16.0/ | https://www.pygmt.org/
+# - GMT 6.5.0 -> https://www.generic-mapping-tools.org/
+# -----------------------------------------------------------------------------
+# Contact
+# - Author: Yvonne Fröhlich
+# - ORCID: https://orcid.org/0000-0002-8566-0619
+# - GitHub: https://github.com/yvonnefroehlich/gmt-pygmt-plotting
+# #############################################################################
+
+
+import netCDF4 as nc
+import numpy as np
+import pygmt as gmt
+
+
+# %%
+# -----------------------------------------------------------------------------
+# General stuff
+# -----------------------------------------------------------------------------
+cmap_nr = "bamako"  # "lapaz"
+color_highlight = "255/90/0"  # ->  orange | URG paper
+radius_earth = 6371  # in km
+
+path_in = "01_in_data"
+path_out = "02_out_figs"
+
+
+for simul_run in ["C"]:  # "A", "B", "C"
+
+    for period in ["high", "low"]:  ## "high", "low"
+        for nr_const in ["low", "high"]:  ## "low", "high"
+            for prem in ["iso", "ani"]:  ## "iso", "ani
+                for ani in ["", "S362ANI_", "3Dani_"]:  ## "", "S362ANI_", "3Dani_"
+                    for nr_type in ["input", "output"]:  ## "input", "output"
+
+                        match simul_run:
+                            case "A":
+                                file_wfs_end = "2000s_virginia"
+                                if ani=="3Dani_": break
+                                if prem=="ani" and ani!="": break
+                                match nr_const:
+                                    case "low": nr_const_set = 50
+                                    case "high": nr_const_set = 100
+                                match period:
+                                    case "low": period_set = 20  # seconds
+                                    case "high": period_set = 50
+                                match nr_type:
+                                    case "input": nr_cmap_max = 50 - 1
+                                    case "output": nr_cmap_max = 50 - 1
+                                # nr_cmap_max = 50 - 1
+                                nr_cb_afg = "a10f5"
+                                histo_nr_max = 100
+                                histo_region_max = 7000
+                                histo_bin_width = 5
+                                histo_frame_xa = 5
+                            case "B":
+                                file_wfs_end = "2500s_100km"
+                                if ani=="S362ANI_": break
+                                if prem=="ani" and ani!="": break
+                                match nr_const:
+                                    case "low": nr_const_set = 5
+                                    case "high": nr_const_set = 33
+                                match period:
+                                    case "low": period_set = "6p3"  # seconds
+                                    case "high": period_set = 20
+                                match nr_type:
+                                    case "input": nr_cmap_max = nr_const_set - 1
+                                    case "output": nr_cmap_max = 33 - 1
+                                # nr_cmap_max = 33 - 1
+                                nr_cb_afg = "a5f1"
+                                histo_nr_max = 33
+                                histo_region_max = 30000
+                                histo_bin_width = 5
+                                histo_frame_xa = 5
+                            case "C":
+                                file_wfs_end = "2000s_100km_JW"
+                                if prem=="ani": break
+                                if ani in ["", "S362ANI_"]: break
+                                match nr_const:
+                                    case "low": break
+                                    case "high": nr_const_set = 1200
+                                match period:
+                                    case "low": period_set = "6p3"  # seconds
+                                    case "high": break
+                                match nr_type:
+                                    case "input": nr_cmap_max = nr_const_set -1
+                                    case "output": nr_cmap_max = 1200 - 1  # nr_cmap_max = 600
+                                # nr_cmap_max = 1200 - 1
+                                nr_cb_afg = "a200f100"
+                                histo_nr_max = 1200
+                                histo_region_max = 300000
+                                histo_bin_width = 50
+                                histo_frame_xa = 100
+
+                        match period_set:
+                            case 50: style_nr = "p1.5p"
+                            case 20: style_nr = "p1.3p"
+                            case "6p3": style_nr = "p0.2p"
+
+
+                        path_wfs = f"{path_in}/runs{simul_run}"
+                        file_wfs = f"scanning_output_{prem}PREM_{ani}" + \
+                                    f"{period_set}s_Nr{nr_const_set}_" + \
+                                    f"{file_wfs_end}.nc"
+                        file_out_basic = f"runs{simul_run}_{period_set}s_" + \
+                                          f"{prem}PREM_{ani}Nr{nr_const_set}"
+
+
+# %%
+# -----------------------------------------------------------------------------
+# Load data of wavefield scanning / learning
+# -----------------------------------------------------------------------------
+                        ds_wfs = nc.Dataset(f"{path_wfs}/{file_wfs}")
+
+                        sz_temp = ds_wfs["pointwise_sz"][:]
+                        sz = np.array(sz_temp)
+
+                        match nr_type:
+                            case "input": nr_str = "starting_Nr_for_scanning"
+                            case "output": nr_str = "pointwise_Nr"
+
+                        Nr_temp = ds_wfs[nr_str][:]
+                        Nr_used = np.array(Nr_temp)
+
+                        Nr_min = np.min(Nr_used)
+                        Nr_max = np.max(Nr_used)
+                        Nr_mean = np.round(np.mean(Nr_used), 1)
+                        Nr_median = np.round(np.median(Nr_used), 1)
+                        Nr_sum = np.sum(Nr_used)
+
+                        array_wfs = np.zeros((len(sz), 3))
+
+                        for i_gp in range(len(sz)):
+                            array_wfs[i_gp, 0] = sz[i_gp, 0] / 1000  # Earth radius km
+                            array_wfs[i_gp, 1] = sz[i_gp, 1] / 1000
+                            array_wfs[i_gp, 2] = Nr_used[i_gp]
+
+
+# %%
+# -----------------------------------------------------------------------------
+# Make geographic map
+# -----------------------------------------------------------------------------
+                        fig = gmt.Figure()
+
+                        gmt.config(
+                            MAP_GRID_PEN_PRIMARY="0.1p,gray50",
+                            FONT="8p",
+                            MAP_FRAME_PEN="0.5p",
+                            MAP_TICK_LENGTH_PRIMARY="3p",
+                        )
+
+                        overrule_bg = False
+                        if nr_type == "output":
+                            gmt.config(
+                                COLOR_BACKGROUND=color_highlight,
+                                COLOR_FOREGROUND=color_highlight,
+                            )
+                            overrule_bg = True
+                        gmt.makecpt(
+                            series=[0, nr_cmap_max, 1],
+                            cmap=cmap_nr,
+                            reverse=True,
+                            overrule_bg=overrule_bg,
+                        )
+
+                        fig.plot(
+                            projection="X6c/12c",
+                            # half circle in Cartesian coordinates
+                            region=[0, radius_earth, -radius_earth, radius_earth],
+                            frame=["WSne", "a1000f500g500", "x+lkm", "y+lkm"],
+                            data=array_wfs,
+                            style=style_nr,
+                            cmap=True,
+                        )
+
+                        fig.colorbar(
+                            frame=[f"x{nr_cb_afg}+l{file_out_basic}_{nr_str[0:9]}","y+lNr"],
+                            position="JBC+h+w5c/0.3c+o-0.5c/1.6c+ef0.3c+ml",
+                        )
+
+                        fig.show()
+                        fig_out = f"{file_out_basic}_{nr_type}_section"
+                        fig_path = f"{path_out}/runs{simul_run}"
+                        # for ext in ["png"]:
+                        #     fig.savefig(fname=f"{fig_path}/{fig_out}.{ext}")
+                        print(fig_out)
+
+
+# %%
+# -----------------------------------------------------------------------------
+# Make histogram
+# -----------------------------------------------------------------------------
+                        fig = gmt.Figure()
+
+                        gmt.config(
+                            FONT="11p",
+                            MAP_FRAME_PEN="0.5p",
+                            MAP_TICK_LENGTH_PRIMARY="3p",
+                        )
+
+                        fig.histogram(
+                            region=[0, histo_nr_max + histo_bin_width, 0, histo_region_max],
+                            data=Nr_used,
+                            frame=["WSne", f"xa{histo_frame_xa}+lNr", "y+lCounts"],  # +a90+e
+                            series=histo_bin_width,
+                            fill="gray80",
+                            pen="0.5p,gray60",
+                            histtype=0,
+                            annotate="+r",
+                        )
+
+                        for i_y, Nr_text in enumerate([
+                            f"{file_out_basic}",
+                            f"{nr_str}",
+                            f"  - Nr_min = {Nr_min}",
+                            f"  - Nr_max = {Nr_max}",
+                            f"  - Nr_mean = {Nr_mean}",
+                            f"  - Nr_median = {Nr_median}",
+                            f"  - Nr_sum = {Nr_sum}",
+                        ]):
+
+                            font_add = ""
+                            fill = "gray97"
+                            if i_y==0:
+                                font_add = ",Helvetica-Bold"
+                                fill = f"{color_highlight}@85"
+                            pen = None
+                            if i_y in [0, 1]:
+                                pen = f"0.2p,{color_highlight}"
+
+                            fig.text(
+                                position="TC",
+                                justify="TL",
+                                font=f"10p{font_add}",
+                                offset=f"1c/{-0.5 - 0.6*i_y}c",
+                                text=Nr_text,
+                                pen=pen,
+                                fill=fill,
+                                clearance="0.1c/0.1c+tO"
+                            )
+
+                        fig.show()
+                        fig_out = f"{file_out_basic}_{nr_type}_histogram"
+                        fig_path = f"{path_out}/runs{simul_run}"
+                        # for ext in ["png"]:
+                        #     fig.savefig(fname=f"{fig_path}/{fig_out}.{ext}")
+                        print(fig_out)
