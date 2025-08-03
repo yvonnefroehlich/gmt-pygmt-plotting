070b7b1f1c646442f90519a31b12a415aa5cea05
Yvonne Fröhlich <94163266+yvonnefroehlich@users.noreply.github.com>
2025-08-03 22:33:04 +0200

    000_general_stuff/01_custom_symbols: Add more custom symbols (#97)

--- 000_general_stuff/01_custom_symbols/custom_symbols.py
+++ 000_general_stuff/01_custom_symbols/custom_symbols.py
@@ -27,23 +27,26 @@ import pygmt as gmt
 
 
 fig = gmt.Figure()
-fig.basemap(region=[-5, 5, -2, 3], projection="X10c/6c", frame=0)
+fig.basemap(region=[-5, 5, -4, 5.5], projection="X10c/6c", frame=0)
 
-fig.plot(x=-3, y=0.5, style="kwindturbine_yf.def/1c", fill="darkgray", pen="black")
-fig.plot(x=-1, y=0.5, style="kwindturbine_yf.def/0.5c", fill="darkgray", pen="black")
-fig.plot(x=1, y=0.5, style="kwindturbine_yf.def/0.5c", fill="black", pen="black")
-fig.plot(x=3, y=0.5, style="kwindturbine_yf.def/1c", fill="lightgray", pen="gray30")
+fig.plot(x=-3, y=1, style="kwindturbine_yf.def/1c", fill="darkgray", pen="black")
+fig.plot(x=-1, y=1, style="kwindturbine_yf.def/0.5c", fill="darkgray", pen="black")
+fig.plot(x=1, y=1, style="kwindturbine_yf.def/0.5c", fill="black", pen="black")
+fig.plot(x=3, y=1, style="kwindturbine_yf.def/1c", fill="lightgray", pen="gray30")
 
 fig.plot(x=-4, y=-1, style="kearthquake_yf.def/1c", fill="darkred")
-fig.plot(x=-2, y=-1, style="kmarker_yf.def/1c", fill="steelblue")
-fig.plot(x=0, y=-1, style="kpinetree_yf.def/1c", fill="darkgreen")
-fig.plot(x=2, y=-1, style="kpoplar_yf.def/1c", fill="seagreen")
-fig.plot(x=4, y=-1, style="ktree_circle_yf.def/1c", fill="lightgreen")
+fig.plot(x=-2, y=-1.5, style="kmarker_yf.def/1c", fill="steelblue")
+fig.plot(x=0, y=-1.5, style="kpinetree_yf.def/1c", fill="darkgreen")
+fig.plot(x=2, y=-1.5, style="kpoplar_yf.def/1c", fill="seagreen")
+fig.plot(x=4, y=-1.5, style="ktree_circle_yf.def/1c", fill="lightgreen")
+
+fig.plot(x=-1, y=-3.5, style="kflower_square_yf.def/1c", fill="purple")
+fig.plot(x=1, y=-3.5, style="kflower_circle_yf.def/1c", fill="darkred")
 
 # Mark plotting points
 fig.plot(
-    x=[-3, -1, 1, 3, -4, -2, 0, 2, 4],
-    y=[0.5, 0.5, 0.5, 0.5, -1, -1, -1, -1, -1],
+    x=[-3, -1, 1, 3, -4, -2, 0, 2, 4, -1, 1],
+    y=[1, 1, 1, 1, -1, -1.5, -1.5, -1.5, -1.5, -3.5, -3.5],
     style="x0.2c",
     fill="255/90/0",
 )
