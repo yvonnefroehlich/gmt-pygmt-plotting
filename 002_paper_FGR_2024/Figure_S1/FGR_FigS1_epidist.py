5b82e382d78c10611f252e16fe74617bd5e8dab4
Yvonne Fröhlich <yvonne.froehlich@kit.edu>
2025-08-14 11:23:16 +0200

    Adjust codes for FigS1 for GitHub

--- 002_paper_FGR_2024/Figure_S1/FGR_FigS1_epidist.py
+++ 002_paper_FGR_2024/Figure_S1/FGR_FigS1_epidist.py
@@ -1,6 +1,6 @@
 # #############################################################################
 # Fröhlich et al. (2024), GJI: Fig. S1
-# Earthquake distribution as epicentral distance plot
+# Earthquake distribution around the Upper Rhine Graben area as epicentral distance plot
 # -----------------------------------------------------------------------------
 # Fröhlich Y., Grund M., Ritter J. R. R. (2024)
 # Lateral and vertical variations of seismic anisotropy in the lithosphere-asthenosphere
@@ -23,9 +23,6 @@
 # #############################################################################
 
 
-import glob
-import os
-
 import numpy as np
 import pandas as pd
 import pygmt as gmt
@@ -35,98 +32,39 @@ import pygmt as gmt
 # -----------------------------------------------------------------------------
 # General stuff
 # -----------------------------------------------------------------------------
-orientation = "vertical"  ## "vertical", "horizontal"
+orientation = "vertical"  ## "vertical" | "horizontal"
 dpi_png = 360
 font_label = "9p,Helvetica-Bold"
 rad_tot = 5.7  # radius
 
-path_in = "01_in_data/"
-path_out = "02_out_figs/"
-
-# Epicenters
-file_sta_in = f"{path_in}stations_info.txt"
-
-# Plate boundaries; Bird 2003
-file_plate_in = f"{path_in}plate_boundaries_Bird_2003.txt"
+path_in = "01_in_data"
+path_out = "02_out_figs"
 
 # Recording stations
-sta_all = "BFO", "WLS", "STU", "ECH", "TMO44", "TMO07"
+file_sta_in = "stations_info.txt"
+# Plate boundaries; Bird 2003
+file_plate_in = "plate_boundaries_Bird_2003.txt"
+# Legend for moment magnitude
+leg_mag_file = "legend_gmt_magitude.txt"
+
+# Recording stations
+sta_all = ["BFO", "WLS", "STU", "ECH", "TMO44", "TMO07"]
 
 # -----------------------------------------------------------------------------
 # Colors
-color_land = "lightgray"
-color_station_marker = "255/215/0"  # "gold"
-color_station_lable = "255/90/0"  # -> orange | URG paper
-color_platebound = "216.750/82.875/24.990"
+color_land = "gray90"
+color_water = "white"
+color_sl = "gray70"
+color_sta = "gold"
+color_hl = "255/90/0"  # -> orange | URG paper
+color_pb = "216.750/82.875/24.990"  # -> darkorange
 
 # -----------------------------------------------------------------------------
-# Box for
-# legend
+cmap_hypo_in = "lajolla"
+hd_max = 500  # hypocentral depth
+
 box_standard = "+gwhite@30+p0.8p,black+r1p"
-# text
 clearance_standard = "0.1c/0.1c+tO"
-
-# -----------------------------------------------------------------------------
-# Colormaps
-# Hypocentral depth colored
-
-hypodepth_max = 700  # hypocentral depth maximum, in km
-pen_epi = "0.01,gray65"
-cb_epi_e = ""
-
-cmap_hypo_in = "lajolla"
-cmap_hypo_out = f"{path_in}{cmap_hypo_in}_resampled_hypo.cpt"
-gmt.makecpt(
-    cmap=cmap_hypo_in,
-    output=cmap_hypo_out,
-    series=[0, hypodepth_max, 23], # 30 colors for [0,700] km see SplitLab
-    # reverse=True,
-)
-cmap_hypo_in_cut = cmap_hypo_out
-cmap_hypo_out_cut = f"{path_in}{cmap_hypo_in}_resampled_cut_hypo.cpt"
-gmt.makecpt(
-    cmap=cmap_hypo_in_cut,
-    output=cmap_hypo_out_cut,
-    truncate="92/598",
-    # SplitLab (5:27) -> 4*23,26*23
-    # this is done before any other operation therefore two-times makecpt
-    series=[0, hypodepth_max],  # 10
-)
-cmap_hypo = cmap_hypo_out_cut
-# ..............................................................................
-hypodepth_max = 500
-pen_epi = "0.01,gray20"
-cb_epi_e = "+ef0.15c"
-
-cmap_hypo_in = "lajolla"
-gmt.makecpt(
-    cmap=cmap_hypo_in,
-    output=cmap_hypo_out,
-    series=[0, hypodepth_max],
-    reverse=True,
-)
-cmap_hypo = cmap_hypo_out
-
-# -----------------------------------------------------------------------------
-# Dictionaries for recording stations
-dict_net = {}
-dict_lat = {}
-dict_lon = {}
-
-station_file = open(file_sta_in,"r")
-lines = station_file.readlines()
-for line in lines[2:]:  # skip header line(s)
-   (net, key, lat, lon, file, label, qstereo, sty, off01, off02, col, rand) = line.split()
-   dict_net[key] = net
-   dict_lat[key] = float(lat)
-   dict_lon[key] = float(lon)
-station_file.close()
-
-
-# %%
-# -----------------------------------------------------------------------------
-# Make geographic map
-# -----------------------------------------------------------------------------
 
 if orientation=="horizontal":
     nrows_use = 2
@@ -136,140 +74,120 @@ elif orientation=="vertical":
     ncols_use = 2
 
 # -----------------------------------------------------------------------------
+# Dictionaries for recording stations
+dict_net = {}
+dict_lat = {}
+dict_lon = {}
+
+station_file = open(f"{path_in}/{file_sta_in}", "r")
+lines = station_file.readlines()
+for line in lines[1:]:  # skip header line(s)
+   (net, key, lon, lat) = line.split()
+   dict_net[key] = net
+   dict_lon[key] = float(lon)
+   dict_lat[key] = float(lat)
+station_file.close()
+
+
+
+# %%
+# -----------------------------------------------------------------------------
+# Make geographic map
+# -----------------------------------------------------------------------------
 fig = gmt.Figure()
 
-gmt.config(
-    FONT_LABEL="10p",  # font size label colorbar
-    FONT_ANNOT_PRIMARY="10p",  # font size annotation colorbar
-    MAP_FRAME_PEN="0.8p",  # thickness of border around scale
-)
+gmt.makecpt(cmap=cmap_hypo_in, series=[0, hd_max])
 
-# -----------------------------------------------------------------------------
 with fig.subplot(
         nrows=nrows_use,
         ncols=ncols_use,
-        subsize=(rad_tot,rad_tot),
+        subsize=(rad_tot, rad_tot),
         frame="lrtb",
         autolabel="(a)",
 ):
 
 # -----------------------------------------------------------------------------
-    for sta in range(6):
-        with fig.set_panel(panel=sta):  # Set panel corresponding to station
+    for i_sta, sta in enumerate(sta_all):
+        center_coord = {"x": dict_lon[sta], "y": dict_lat[sta]}
+
+        file_epi_in = f"{sta}_epi_swsm_all.txt"
+        df_epi_raw = pd.read_csv(
+            f"{path_in}/{file_epi_in}",
+            delimiter=" ",
+            names=["longitude", "latitude", "magnitude", "hdepth"],
+        )
+        df_epi = df_epi_raw
+        df_epi.magnitude = np.exp(df_epi_raw.magnitude / 1.7) * 0.0035
 
 # -----------------------------------------------------------------------------
+        with fig.set_panel(i_sta):  # Set panel corresponding to station
             # Make azimuthal equistant plots / projection direct in GMT
             # azimuthal equidistant projection
             # - elon0/lat0[/horizon]/scale
             # - Elon0/lat0[/horizon]/width
             # - horizon max. distance to the projection center
             #   i.e. the visible portion of the rest of the world map
-            #   in degrees <= 180Â° (default 180Â°)
+            #   in degrees <= 180 deg (default 180 deg)
+            projection = f"E{dict_lon[sta]}/{dict_lat[sta]}/170/?"
+            fig.basemap(region="d", projection=projection, frame=0)
+
             fig.coast(
-                region="d",
-                projection=f"E{dict_lon[sta_all[sta]]}/{dict_lat[sta_all[sta]]}/170/?",
-                # projection="N?",
                 area_thresh="50000",
                 resolution="c",
-                shorelines="1/0.1p,darkgray",
+                shorelines="1/0.1p,{color_sl}",
                 land=color_land,
-                water="white",
-                frame="f",
+                water=color_water,
             )
 
-# -----------------------------------------------------------------------------
             # Plot plate boundaries
-            fig.plot(data=file_plate_in, pen=f"0.5p,{color_platebound}")
+            fig.plot(data=f"{path_in}/{file_plate_in}", pen=f"0.5p,{color_pb}")
 
 # -----------------------------------------------------------------------------
-            # Highlight Epicentral distance range used in this study
-            epi_min = 90
-            epi_max = 150
-
-            # circles
-            fig.plot(
-                x=dict_lon[sta_all[sta]],
-                y=dict_lat[sta_all[sta]],
-                style=f"E-{epi_min*2}+d",  # 2 x 90 deg
-                pen="1p,gray50,-",
-            )
-            fig.plot(
-                x=dict_lon[sta_all[sta]],
-                y=dict_lat[sta_all[sta]],
-                style=f"E-{epi_max*2}+d",  # 2 x 150 deg
-                pen="1p,gray50,-",
-            )
-
-            # Add label for annotations limits of epicentral distance range
-            fig.text(
-                x=dict_lon[sta_all[sta]],
-                y=-28,
-                text=f"{epi_min}@.",
-                font="9p,black",
-            )
-            fig.text(
-                x=dict_lon[sta_all[sta]],
-                y=-88,
-                text=f"{epi_max}@.",
-                font="9p,black",
-            )
+            # HighlighteEpicentral distance range used in this study
+            for epi_dist, y in zip([90, 150], [-28, -88]):
+                # Plot circles
+                fig.plot(style=f"E-{epi_dist * 2}+d", pen="1p,gray50,4_2", **center_coord)
+                # Add label for annotations limits of epicentral distance range
+                fig.text(x=center_coord["x"], y=y, text=f"{epi_dist}@.", font="9p,black")
 
 # -----------------------------------------------------------------------------
             # Plot epicenters
-            file_epi_in = f"{path_in}{sta_all[sta]}_epi_swsm_all.txt"
-            df_epi_raw = pd.read_csv(
-                file_epi_in,
-                delimiter=" ",
-                names=["longitude", "latitude", "magnitude", "hdepth"],
-            )
-            df_epi = df_epi_raw
-            df_epi.magnitude = np.exp(df_epi_raw.magnitude / 1.7) * 0.0035
-
             fig.plot(
                 x=df_epi.longitude,
                 y=df_epi.latitude,
-                style="c",
                 size=df_epi.magnitude,
                 fill=df_epi.hdepth,
-                cmap=cmap_hypo,
-                pen=pen_epi,
+                style="c",
+                cmap=True,
+                pen="0.01,gray20",
             )
 
 # -----------------------------------------------------------------------------
             # Plot recording station
             # marker
-            fig.plot(
-                x=dict_lon[sta_all[sta]],
-                y=dict_lat[sta_all[sta]],
-                style="i0.4",
-                fill=color_station_marker,
-                pen="0.6p,black"
-            )
+            fig.plot(style="i0.4", fill=color_sta, pen="0.6p,black", **center_coord)
             # label
             fig.text(
-                x=dict_lon[sta_all[sta]],
-                y=dict_lat[sta_all[sta]],
-                text=sta_all[sta],
+                text=sta,
                 offset="0c/-0.55c",
-                font=f"{font_label},{color_station_lable}",
+                font=f"{font_label},{color_hl}",
                 fill="white@30",
-                pen=f"0.8p,{color_station_lable}",
+                pen=f"0.8p,{color_hl}",
                 clearance=clearance_standard,
+                **center_coord,
             )
 
 # -----------------------------------------------------------------------------
 # Add colorbar for hypocentral depth
-fig.colorbar(
-    cmap=cmap_hypo,
-    position=f"JBR+jBR+w4.5c/0.3+o0.7c/-1c+h+ml{cb_epi_e}",
-    frame="xa100f50+lhypocentral depth / km",
-)
+with gmt.config(FONT="15p"):
+    fig.colorbar(
+        position="JBR+jBR+w4.5c/0.3+o0.7c/-1c+h+ml+ef0.15c",
+        frame="xa100f50+lhypocentral depth /km",
+    )
 
-# -----------------------------------------------------------------------------
 # Add legend for moment magnitude
-leg_mag_file = "legend_gmt_magitude.txt"
 fig.legend(
-    spec=f"{path_in}{leg_mag_file}",
+    spec=f"{path_in}/{leg_mag_file}",
     position="JBL+jBL+w4.5c+o0.6c/-1.2c",
     box=box_standard,
 )
@@ -277,9 +195,7 @@ fig.legend(
 # -----------------------------------------------------------------------------
 # Show and save the figure
 fig.show()
-
-fig_name = f"{path_out}/paperURG_SWS_epi_ALL_{orientation}"
-# for ext in ["png", "pdf", "eps"]:
-#     fig.savefig(fname=f"{fig_name}.{ext}")
-
+fig_name = f"paperURG_SWS_epi_ALL_{orientation}"
+for ext in ["png"]:  #, "pdf", "eps"]:
+    fig.savefig(fname=f"{path_out}/{fig_name}.{ext}")
 print(fig_name)
