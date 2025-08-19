7e4c6eb9b2bad51bfc42622c7d0ed2d7a11d439c
Yvonne Fröhlich <yvonne.froehlich@kit.edu>
2025-08-19 08:45:50 +0200

    Set up folder structur

---
+++ 010_axisem/07_seismograms/seismograms.py
@@ -0,0 +1,17 @@
+# #############################################################################
+# Plot seismograms simulated with AxiSEM3D with PyGMT
+# -----------------------------------------------------------------------------
+# History
+# - Created: -
+# - Updatd: 2025/08/19 - adjust for GitHub
+# -----------------------------------------------------------------------------
+# Versions
+# - PyGMT v0.16.0 -> https://www.pygmt.org/v0.16.0/ | https://www.pygmt.org/
+# - GMT 6.5.0 -> https://www.generic-mapping-tools.org/
+# -----------------------------------------------------------------------------
+# Contact
+# - Author: Yvonne Fröhlich
+# - ORCID: https://orcid.org/0000-0002-8566-0619
+# - GitHub: https://github.com/yvonnefroehlich/gmt-pygmt-plotting
+# #############################################################################
+
