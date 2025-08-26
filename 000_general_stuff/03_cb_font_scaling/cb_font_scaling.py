cff6ffeb49171a81a47753fa30cdd34426b25f8e
Yvonne Fröhlich <yvonne.froehlich@kit.edu>
2025-08-26 18:43:50 +0200

    Update codes

--- 000_general_stuff/03_cb_font_scaling/cb_font_scaling.py
+++ 000_general_stuff/03_cb_font_scaling/cb_font_scaling.py
@@ -22,15 +22,13 @@
 import pygmt
 import numpy as np
 
-
 plot_size = 5
-font_size = 6
+font_size = 7
 
 cb1_width = 4
-cb2_width = 1.5
+cb2_width = 2
 cb1_label = "cb1 is long"
 cb2_label = "cb2 is short"
-
 
 # -----------------------------------------------------------------------------
 def scale_cb_font(cb_width):
@@ -39,27 +37,26 @@ def scale_cb_font(cb_width):
     return font_scaling
 # -----------------------------------------------------------------------------
 
-
-# -----------------------------------------------------------------------------
 fig = pygmt.Figure()
-with pygmt.config(FONT=font_size):
-    fig.basemap(region=[-plot_size, plot_size] * 2, projection=f"X{plot_size}c", frame=1)
+
+# (Left)
+pygmt.config(FONT=font_size)
+fig.basemap(region=[-plot_size, plot_size] * 2, projection=f"X{plot_size}c", frame=1)
 
 fig.colorbar(cmap="navia", position=f"jTC+w{cb1_width}c+h", frame=f"x+l{cb1_label}")
 fig.colorbar(cmap="navia", position=f"jBC+w{cb2_width}c+h", frame=f"x+l{cb2_label}")
 
-# -----------------------------------------------------------------------------
 fig.shift_origin(xshift="6c")
+
+# (Right)
 with pygmt.config(FONT=font_size):
     fig.basemap(region=[-plot_size, plot_size] * 2, projection=f"X{plot_size}c", frame=1)
 
-font_scaling = scale_cb_font(cb1_width)
-with pygmt.config(FONT=f"{font_size / font_scaling}"):
+font_scaling1 = scale_cb_font(cb1_width)
+with pygmt.config(FONT=f"{font_size / font_scaling1}"):
     fig.colorbar(cmap="navia", position=f"jTC+w{cb1_width}c+h", frame=f"x+l{cb1_label}")
-
-font_scaling = scale_cb_font(cb2_width)
-with pygmt.config(FONT=f"{font_size / font_scaling}"):
+font_scaling2 = scale_cb_font(cb2_width)
+with pygmt.config(FONT=f"{font_size / font_scaling2}"):
     fig.colorbar(cmap="navia", position=f"jBC+w{cb2_width}c+h", frame=f"x+l{cb2_label}")
 
-# -----------------------------------------------------------------------------
 fig.show()
