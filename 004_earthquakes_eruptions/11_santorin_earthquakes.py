df3d8fa6b7c929551e96953c1564792f491704b2
Yvonne Fröhlich <yvonne.froehlich@kit.edu>
2025-09-23 00:24:01 +0200

    Update santorini content

--- 004_earthquakes_eruptions/11_santorin_earthquakes.py
+++ 004_earthquakes_eruptions/11_santorin_earthquakes.py
@@ -48,18 +48,21 @@ path_out = "02_out_figs"
 # Colors
 color_sl = "gray10"
 color_hl = "255/90/0"  # -> orange
-color_water = "gray"  # "lightblue"  # "steelblue@50"
+color_water = "gray"
 
 # Plotting region
-coord_red = 0.2
+coord_red = 0.1
 region = [
-    min_lon + coord_red, max_lon - coord_red, min_lat + coord_red, max_lat - coord_red
+    min_lon + coord_red / 2,
+    max_lon - coord_red / 2,
+    min_lat + coord_red,
+    max_lat - coord_red / 2,
 ]
 
 
 # %%
 # -----------------------------------------------------------------------------
-# Download earthquake data
+# Download USGS earthquake data
 # -----------------------------------------------------------------------------
 # Set up request using the webservice
 # see https://earthquake.usgs.gov/fdsnws/event/1/
@@ -108,8 +111,8 @@ df_eq_cmt_mod = df_eq_cmt
 df_eq_cmt_mod["depth_km"] = df_eq_cmt_raw["depth"]
 
 # https://doi.org/10.5281/zenodo.15111649
-df_eq_external_raw = pd.read_csv(f"{path_in}/Santorini_catalog.csv", sep=",")
-df_eq_external = df_eq_external_raw[df_eq_external_raw["magnitude"] < 11]
+df_eq_external_raw = pd.read_csv(f"{path_in}/catalog_santorini.csv", sep=",")
+df_eq_external = df_eq_external_raw[df_eq_external_raw["year"] > 2024]
 
 # Download elevation grid
 grid = gmt.datasets.load_earth_relief(resolution="03s", region=region)
@@ -121,84 +124,65 @@ grid = "@earth_relief_03s"
 # Make map of epicenters with different color-codings
 # -----------------------------------------------------------------------------
 
-for i_coord_red, coord_red in enumerate(np.arange(-0.2, 0.35 + 0.05, 0.05)):
+fig = gmt.Figure()
 
-    region = [
-        min_lon + coord_red / 2,
-        max_lon - coord_red / 2,
-        min_lat + coord_red,
-        max_lat - coord_red / 2,
-    ]
+for cmap, reverse, series, fill_usgs, fill_cmt, fill_external, frame, label in zip(
+    ["lipari", "lajolla"],
+    [True, False],
+    [[3, 5], [3, 15]],
+    [df_eq_usgs.mag, df_eq_usgs.depth],
+    [df_eq_cmt.magnitude, df_eq_cmt.depth_km],
+    [df_eq_external.magnitude, df_eq_external.depth],
+    [["af", "WSne"], ["af", "wSne"]],
+    ["magnitude", "hypocentral depth / km"],
+):
 
-    fig = gmt.Figure()
+    fig.basemap(projection="M12c", region=region, frame=frame)
 
-    # for cmap, series, fill_usgs, fill_cmt, fill_external, frame, label in zip(
-    #     ["batlow", "navia", "oslo"],
-    # #    [[1, 5], [0, 11], ["2025-01-26T00:00:00.000Z", "2025-02-10T00:00:00.000Z"]],
-    #     [[4, 5], [6, 11], [1, 31]],
-    #     [df_eq_usgs.mag, df_eq_usgs.depth, df_eq_usgs.time],
-    #     [df_eq_cmt.magnitude, df_eq_cmt.depth_km, df_eq_cmt.day],
-    #     [df_eq_external.magnitude, df_eq_external.depth, df_eq_external.day],
-    #     [["af", "WSne"], ["af", "wSne"], ["af", "wSnE"]],
-    #     ["magnitude", "hypocentral depth / km", "date"],
-    # ):
+    # Elevation
+    gmt.makecpt(cmap="oleron", series=[0, 1500])
+    fig.grdimage(grid=grid, region=region, cmap=True, shading=True)
+    fig.coast(water=color_water, shorelines=f"1/0.5p,{color_sl}")
+    if cmap == "navia" and coord_red == 0:  # Plot elevation colorbar only once
+        fig.colorbar(frame="x+lelevation / m", position="+o0c/-0.85c+ef0.3c+ml")
 
-    for cmap, reverse, series, fill_usgs, fill_cmt, fill_external, frame, label in zip(
-        ["lipari", "lajolla"],
-        [True, False],
-        [[3, 5], [3, 15]],
-        [df_eq_usgs.mag, df_eq_usgs.depth],
-        [df_eq_cmt.magnitude, df_eq_cmt.depth_km],
-        [df_eq_external.magnitude, df_eq_external.depth],
-        [["af", "WSne"], ["af", "wSne"]],
-        ["magnitude", "hypocentral depth / km"],
-    ):
+    # Mark Santorini
+    fig.plot(x=25.43, y=36.42, style="x0.6c", pen=f"5p,{color_hl}")
 
-        fig.basemap(projection="M12c", region=region, frame=frame)
+    # Epicenters
+    gmt.makecpt(cmap=cmap, series=series, reverse=reverse)
+    fig.colorbar(frame=f"x+l{label}", position="+o0c/1.5c+e0.3c+ml")
+    # Mw all
+    fig.plot(
+        x=df_eq_external.longitude,
+        y=df_eq_external.latitude,
+        style="c0.07c",
+        fill=fill_external,
+        cmap=True,
+    )
+    # Mw >= 3.9
+    # fig.plot(
+    #     x=df_eq_usgs.longitude,
+    #     y=df_eq_usgs.latitude,
+    #     style="c0.07c",
+    #     fill=fill_usgs,
+    #     cmap=True,
+    # )
+    # CMT
+    # df_eq_cmt_mod["depth"] = fill_cmt
+    # fig.meca(
+    #     spec=df_eq_cmt_mod,
+    #     scale="0.45c",
+    #     outline="0.1p,gray50",
+    #     cmap=True,
+    # )
 
-        # Elevation
-        gmt.makecpt(cmap="oleron", series=[0, 1500])
-        fig.grdimage(grid=grid, region=region, cmap=True, shading=True)
-        fig.coast(water=color_water, shorelines=f"1/0.5p,{color_sl}")
-        if cmap == "navia" and coord_red == 0:  # Plot elevation colorbar only once
-            fig.colorbar(frame="x+lelevation / m", position="+o0c/-0.85c+ef0.3c+ml")
+    fig.shift_origin(xshift="w+0.5c")
 
-        # Mark Santorini
-        fig.plot(x=25.43, y=36.42, style="x0.6c", pen=f"5p,{color_hl}")
+fig.show() # method="external")
 
-        # Epicenters
-        gmt.makecpt(cmap=cmap, series=series, reverse=reverse)
-        fig.colorbar(frame=f"x+l{label}", position="+o0c/1.5c+e0.3c+ml")
-        # Mw all
-        fig.plot(
-            x=df_eq_external.longitude,
-            y=df_eq_external.latitude,
-            style="c0.07c",
-            fill=fill_external,
-            cmap=True,
-        )
-        # Mw >= 3.9
-        # fig.plot(
-        #     x=df_eq_usgs.longitude,
-        #     y=df_eq_usgs.latitude,
-        #     style="c0.07c",
-        #     fill=fill_usgs,
-        #     cmap=True,
-        # )
-        # CMT
-        # df_eq_cmt_mod["depth"] = fill_cmt
-        # fig.meca(
-        #     spec=df_eq_cmt_mod,
-        #     scale="0.45c",
-        #     outline="0.1p,gray50",
-        #     cmap=True,
-        # )
-
-        fig.shift_origin(xshift="w+0.5c")
-
-    fig.show() # method="external")
-
-    fig_name = f"11_{i_coord_red + 1}_santorini_earthquake_" + str(coord_red)[0:4] + "deg"
-    for ext in ["png", "pdf"]:  # "pdf", "eps"
-        fig.savefig(fname=f"{path_out}/{fig_name}.{ext}")
-    print(fig_name)
+# fig_name = f"11_{i_coord_red + 1}_santorini_earthquake_" + str(coord_red)[0:4] + "deg"
+fig_name = "11_santorini_earthquake_" + str(coord_red)[0:4] + "deg"
+for ext in ["png"]:  # "pdf", "eps"
+    fig.savefig(fname=f"{path_out}/{fig_name}.{ext}")
+print(fig_name)
