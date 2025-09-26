914228a20c8089a1c542c8397592d6d48b54ce22
Yvonne Fröhlich <yvonne.froehlich@kit.edu>
2025-09-26 22:35:57 +0200

    Update header

--- 015_santorini_earthquakes/santorini_earthquakes.py
+++ 015_santorini_earthquakes/santorini_earthquakes.py
@@ -9,12 +9,12 @@
 # - https://ezgif.com/pdf-to-gif (last accessed 2025/09/25)
 #
 # Data taken from
-# - Karakostas V, Lomax A, Anagnostou V, Papadimitriou E, Acoccela V, & Hicks S (2025).
+# - Karakostas V, Lomax A, Anagnostou V, Papadimitriou E, Acoccela V, Hicks S (2025).
 #   2025 Santorini–Amorgos NLL-SSST-coherence high-precision relocated earthquake
 #   catalog. Zenodo. https://doi.org/10.5281/zenodo.15111649 .
 #
 # More inforamtion
-# - Isken M P, Karstens J, Nomikou P et al. (2025) Volcanic crisis reveals coupled
+# - Isken M P, Karstens J, Nomikou P, et al. (2025) Volcanic crisis reveals coupled
 #   magma system at Santorini and Kolumbo. Nature, 645:939–945.
 #   https://doi.org/10.1038/s41586-025-09525-7 .
 # - https://www.geomar.de/en/news/article/magmaverlagerung-loeste-zehntausende-erdbeben-aus
