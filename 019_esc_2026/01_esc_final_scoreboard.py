2972d45fcbdfc479ddf5d58a98e0f2188f24b725
Yvonne Fröhlich <94163266+yvonnefroehlich@users.noreply.github.com>
2026-05-20 14:46:07 +0200

    019_esc_2026: Add first version of scoreboard and analysis (#176)

---
+++ 019_esc_2026/01_esc_final_scoreboard.py
@@ -0,0 +1,194 @@
+# #############################################################################
+# Final of the Eurovision Song Contest 2026 in Austria
+# - Points by countries plotted as scoreboards
+# - Color-coding for jury, public, and total points
+# Data
+# - modified from https://eurovisionworld.com/eurovision/2026
+# - last accessed: 2026/05/18
+# -----------------------------------------------------------------------------
+# History
+# - Created: 2026/05/18
+# -----------------------------------------------------------------------------
+# Versions
+# - PyGMT v0.18.0 -> https://www.pygmt.org
+# - GMT 6.6.0 -> https://www.generic-mapping-tools.org
+# -----------------------------------------------------------------------------
+# Contact
+# - Author: Yvonne Fröhlich
+# - ORCID: https://orcid.org/0000-0002-8566-0619
+# - GitHub: https://github.com/yvonnefroehlich/gmt-pygmt-plotting
+# #############################################################################
+
+
+import numpy as np
+import pandas as pd
+import pygmt
+
+# %%
+# -----------------------------------------------------------------------------
+# Choose for your needs
+# -----------------------------------------------------------------------------
+year = 2026
+group = "public"  ## "jury" | "public" | "total"
+add_numbers = False  ## False | True
+
+
+# %%
+# -----------------------------------------------------------------------------
+# General stuff
+# -----------------------------------------------------------------------------
+path_in = "01_in_data"
+path_out = "02_out_figs"
+
+color_hl = "255/90/0"  # -> orange | URG paper
+
+max_points = 12
+cb_gap = 0.25
+x_nan_offset = 0.9
+if group == "total":
+    max_points = 24
+    cb_gap = 0.1
+    x_nan_offset = 0.96
+
+
+# %%
+# -----------------------------------------------------------------------------
+# Load and prepare data
+# -----------------------------------------------------------------------------
+df_sb = pd.read_csv(f"{path_in}/esc_{year}_scoreboard_{group}.txt", delimiter="\t")
+
+country_recieve = df_sb["country"].to_numpy().tolist()
+country_recieve.sort()
+
+country_give = df_sb.columns.tolist()[3 : len(df_sb.columns.tolist()) - 1]
+country_give.sort()
+country_give.append("Rest of world")
+
+x = np.arange(0, len(country_give), 1)
+
+
+# %%
+# -----------------------------------------------------------------------------
+# Make plot for scoreboards
+# -----------------------------------------------------------------------------
+fig = pygmt.Figure()
+fig.basemap(
+    region=[-0.6, len(country_give) - 1 + 0.6, -0.6, 24.6],
+    projection="X15c/-10c",
+    frame="wsne",
+)
+
+pygmt.makecpt(cmap="oslo", series=[0, max_points + 1, 1], reverse=True)
+
+country_place = []
+country_sum = []
+for i_country_temp, country_temp in enumerate(country_recieve):
+    df_sb_temp = df_sb[df_sb["country"] == country_temp]
+    # TODO: FutureWarning by pandas: Setting an item of incompatible dtype is
+    # deprecated and will raise in a future error of pandas. Value 'nan' has
+    # dtype incompatible with int64, please explicitly cast to a compatible
+    # dtype first.
+    df_sb_temp.loc[:,country_temp] = np.nan  # A country can not vote for itself
+
+    array_sb_temp = df_sb_temp[country_give].to_numpy().squeeze()
+    y = [i_country_temp] * len(array_sb_temp)
+
+    # Plot squares with color-coding for points
+    fig.plot(
+        x=x,
+        y=y,
+        fill=array_sb_temp,
+        style="s0.6c",
+        pen="0.1p,gray90",
+        cmap=True,
+    )
+
+# -----------------------------------------------------------------------------
+    # Converting NaN to int leads to -9223372036854775808
+    # Int64 does not work as proposed by the docs ???
+    # TODO: FutureWarning by pandas: Setting an item of incompatible dtype is
+    # deprecated and will raise in a future error of pandas. Value 'nan' has
+    # dtype incompatible with int64, please explicitly cast to a compatible
+    # dtype first.
+    if add_numbers:
+        array_sb_temp_int = array_sb_temp.astype(int)
+        list_sb_temp_int = array_sb_temp_int.tolist()
+        for i_value, value in enumerate(list_sb_temp_int):
+            if value == -9223372036854775808:
+                list_sb_temp_int[i_value] = "X"
+
+        # Add number of points as text in the middle of the squares
+        fig.text(
+            text=list_sb_temp_int,
+            x=x,
+            y=y,
+            font=f"5p,1,{color_hl}",
+        )
+
+# -----------------------------------------------------------------------------
+    # Build list for y axis
+    place_temp = df_sb_temp["place"].to_numpy().squeeze()
+    country_place.append(place_temp)
+    sum_temp = df_sb_temp["sum"].to_numpy().squeeze()
+    country_sum.append(sum_temp)
+
+# -----------------------------------------------------------------------------
+# Text for x axis
+fig.text(
+    text=country_give,
+    x=x,
+    y=[-1] * len(country_give),
+    angle=90,
+    justify="LM",
+    font="8p,black",
+    no_clip=True,
+)
+
+# Text for y axis
+for text_recieve, x_text in zip(
+    [country_sum, country_place, country_recieve], [-1, -3, -4.5]
+):
+    fig.text(
+        text=text_recieve,
+        x=[x_text] * len(country_recieve),
+        y=df_sb.index.tolist(),
+        justify="RM",
+        font="8p,black",
+        no_clip=True,
+    )
+
+# Label for event, year and included voting group
+for text_label, y_offset, font_size in zip(
+    ["ESC", f"{year} | {group}"], [1.1, 0.65], [11, 11.5]
+):
+    fig.text(
+        text=text_label,
+        position="TL",
+        justify="LM",
+        offset=f"-2.6c/{y_offset}c",
+        font=f"{font_size}p,1,{color_hl}",
+        no_clip=True,
+    )
+
+# -----------------------------------------------------------------------------
+# Add colorbar
+with pygmt.config(MAP_FRAME_PEN="0.1p,gray50"):
+    fig.colorbar(
+        frame="y+lpoints", equalsize=cb_gap, position="+nno permission to vote"
+    )
+
+if add_numbers:
+    fig.text(
+        text="X",
+        position="BL",
+        offset=f"{x_nan_offset}c/-0.99c",
+        justify="MC",
+        font="8p,black",
+        no_clip=True,
+    )
+
+# -----------------------------------------------------------------------------
+fig.show()
+fig_name = f"esc_final_{year}_scoreboard_{group}"
+# for ext in ["png"]: #, "pdf"]:
+#     fig.savefig(fname=f"{path_out}/{fig_name}.{ext}")
---
+++ 019_esc_2026/02_esc_final_analysis.py
@@ -0,0 +1,259 @@
+# #############################################################################
+# Final of the Eurovision Song Contest 2026 in Austria
+# - Analysis comparing jury and public points
+# Data
+# - modified from https://eurovisionworld.com/eurovision/2026
+# - last accessed: 2026/05/18
+# -----------------------------------------------------------------------------
+# History
+# - Created: 2026/05/18
+# -----------------------------------------------------------------------------
+# Versions
+# - PyGMT v0.18.0 -> https://www.pygmt.org
+# - GMT 6.6.0 -> https://www.generic-mapping-tools.org
+# -----------------------------------------------------------------------------
+# Contact
+# - Author: Yvonne Fröhlich
+# - ORCID: https://orcid.org/0000-0002-8566-0619
+# - GitHub: https://github.com/yvonnefroehlich/gmt-pygmt-plotting
+# #############################################################################
+
+
+import numpy as np
+import pandas as pd
+import pygmt
+
+# %%
+# -----------------------------------------------------------------------------
+# Choose for your needs
+# -----------------------------------------------------------------------------
+year = 2026
+
+
+# %%
+# -----------------------------------------------------------------------------
+# General stuff
+# -----------------------------------------------------------------------------
+path_in = "01_in_data"
+path_out = "02_out_figs"
+fig_name = f"{path_out}/esc_final_{year}"
+
+projection = "X17c/12c"
+
+max_points = 550
+offset_y_country = max_points - 150  # manually adjusted
+
+color_total = "orange"
+color_jury = "cyan3"
+color_public = "darkblue"
+
+symbol_total = "c0.2c"
+symbol_jury = "i0.25c"
+symbol_public = "t0.25c"
+symbol_diff = "s0.25c"
+
+box_standard = "+gwhite+p0.1p,gray30+r2p"
+
+
+# %%
+# -----------------------------------------------------------------------------
+# Load and prepare data
+# -----------------------------------------------------------------------------
+df_esc = pd.read_csv(f"{path_in}/esc_{year}.txt", delimiter="\t")
+
+# -----------------------------------------------------------------------------
+# Add column for difference between public and jury points
+diff_public_jury = df_esc["public"] - df_esc["jury"]
+df_esc["diff_public_jury"] = diff_public_jury
+df_esc_diff_neg = df_esc[df_esc["diff_public_jury"] < 0]
+df_esc_diff_pos = df_esc[df_esc["diff_public_jury"] > 0]
+
+# -----------------------------------------------------------------------------
+# Add column for places based on public and jury points separately
+df_esc = df_esc.sort_values(by=["jury"], ignore_index=True, ascending=False)
+df_esc["place_jury"] = np.arange(1, len(df_esc) + 1, 1)
+
+df_esc = df_esc.sort_values(by=["public"], ignore_index=True, ascending=False)
+df_esc["place_public"] = np.arange(1, len(df_esc) + 1, 1)
+
+
+# %%
+# -----------------------------------------------------------------------------
+# Points total, jury, public
+# -----------------------------------------------------------------------------
+fig = pygmt.Figure()
+pygmt.config(MAP_GRID_PEN_PRIMARY="0.01p,gray50")
+
+fig.basemap(
+    region=[0.5, len(df_esc) + 0.5, 0, max_points],
+    projection=projection,
+    frame=[
+        "xa1g1+lplace based on total points",
+        "ya100f20+ltotal / jury / public points",
+    ],
+)
+
+# Plot total, jury and public points
+for group, style, color in zip(
+    ["total", "jury", "public"],
+    [symbol_total, symbol_jury, symbol_public],
+    [color_total, color_jury, color_public],
+):
+    label = group
+    if group == "total":
+        label = f"{group}+HESC final {year}+f10p"
+
+    fig.plot(
+        data=df_esc[["place", group]],
+        style=style,
+        pen="0.5p,gray30",
+        fill=color,
+        label=label,
+        no_clip=True,
+    )
+
+# Add text for total points
+fig.text(
+    text=df_esc["total"],
+    x=df_esc["place"],
+    y=df_esc["total"],
+    justify="MC",
+    offset="0c/0.3c",
+    font="9p",
+    fill="white@50",
+)
+# Add text for countries
+fig.text(
+    text=df_esc["country"],
+    x=df_esc["place"],
+    y=np.ones(len(df_esc)) * offset_y_country,
+    justify="LM",
+    angle=90,
+    font="9p",
+    fill="white@30",
+)
+
+# Add legend
+fig.legend(position="jRM+o1c/0c+w2.7c", box=box_standard)
+
+# for ext in ["png"]:  # , "pdf", "eps"]
+#     fig.savefig(fname=f"{fig_name}_points.{ext}")
+fig.show()
+
+
+# %%
+# -----------------------------------------------------------------------------
+# Places based on public and jury points separately
+# -----------------------------------------------------------------------------
+for points_sep in ["public", "jury"]:
+    match points_sep:
+        case "jury":
+            color_sep = color_jury
+            symbol_sep = symbol_jury
+        case "public":
+            color_sep = color_public
+            symbol_sep = symbol_public
+
+    fig = pygmt.Figure()
+    pygmt.config(MAP_GRID_PEN_PRIMARY="0.01p,gray50")
+
+    fig.basemap(
+        region=[0.5, len(df_esc) + 0.5, 0, max_points],
+        projection=projection,
+        frame=[
+            f"xa1g1+lplace based {points_sep} points",
+            f"ya100f20+l{points_sep} points",
+        ],
+    )
+    # Plot public / jury points
+    fig.plot(
+        data=df_esc[[f"place_{points_sep}", points_sep]],
+        style=symbol_sep,
+        pen="0.5p,gray30",
+        fill=color_sep,
+        label=points_sep,
+        no_clip=True,
+    )
+
+    # Add text for public / jury points
+    fig.text(
+        text=df_esc[points_sep],
+        x=df_esc[f"place_{points_sep}"],
+        y=df_esc[points_sep],
+        justify="MC",
+        offset="0c/0.35c",
+        font="9p",
+        fill="white@50",
+    )
+    # Add text for countries
+    fig.text(
+        text=df_esc["country"],
+        x=df_esc[f"place_{points_sep}"],
+        y=np.ones(len(df_esc)) * offset_y_country,
+        justify="LM",
+        angle=90,
+        font="9p",
+        fill="white@30",
+    )
+
+    # for ext in ["png"]:  # , "pdf", "eps"]
+    #     fig.savefig(fname=f"{fig_name}_{points_sep}.{ext}")
+    fig.show()
+
+
+# %%
+# -----------------------------------------------------------------------------
+# Difference between jury and public points
+# -----------------------------------------------------------------------------
+fig = pygmt.Figure()
+pygmt.config(MAP_GRID_PEN_PRIMARY="0.01p,gray50")
+
+fig.basemap(
+    region=[0.5, len(df_esc) + 0.5, -max_points / 2, max_points / 2],
+    projection=projection,
+    frame=[
+        "xa1g1+lplace based on total points",
+        "ya100f20+ldifference between public and jury points",
+    ],
+)
+
+# Plot zero line
+fig.plot(x=[0, 27], y=[0, 0], pen="1p,gray30")
+
+# Difference
+for df_diff, color, y_offset in zip(
+    [df_esc_diff_neg, df_esc_diff_pos], [color_jury, color_public], [-0.35, 0.35]
+):
+    # Plot differences
+    fig.plot(
+        x=df_diff["place"],
+        y=df_diff["diff_public_jury"],
+        style=symbol_diff,
+        pen="0.5p,gray30",
+        fill=color,
+    )
+    # Add text for differences
+    fig.text(
+        text=df_diff["diff_public_jury"],
+        x=df_diff["place"],
+        y=df_diff["diff_public_jury"],
+        justify="MC",
+        offset=f"0c/{y_offset}c",
+        font="9p",
+        fill="white@50",
+    )
+
+# Add text for countries
+fig.text(
+    text=df_esc["country"],
+    x=df_esc["place"],
+    y=np.ones(len(df_esc)) * (-max_points / 2 + 10),
+    justify="LM",
+    angle=90,
+    font="9p",
+    fill="white@30",
+)
+
+# for ext in ["png"]:  # , "pdf", "eps"]
+#     fig.savefig(fname=f"{fig_name}_diff.{ext}")
+fig.show()
